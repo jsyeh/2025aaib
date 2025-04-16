5f73da5e3b6ca5f0f104ae530ee4cbcba044d633
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2025-04-16 12:25:29 +0800

    week09

---
+++ week09-3.cpp
@@ -0,0 +1,17 @@
+// week09-3.cpp
+// YEH001_Star
+#include <stdio.h>
+int main()
+{
+    printf("請輸入大小: ");
+	int n;
+	scanf("%d", &n);
+
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			printf("*");
+		}
+		//printf("i:%d \n", i);
+		printf("\n");
+	}
+}
